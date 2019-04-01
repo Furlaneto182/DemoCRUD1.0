ac0da3e184cff283a77644e265abd951930b960c
Felipe Furlaneto de Sousa <felipebiofuels@gmail.com>
2019-04-01 12:06:25 -0300

    Add project files.

---
+++ DemoCRUD/Models/Livro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoCRUD.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoEdicao { get; set; }
+        public decimal Valor { get; set; }
+
+        //Associañdo a classe gênero a livro
+        public Genero Genero { get; set; }
+        public Genero GeneroId { get; set; }
+        //Desta forma, o entity entende que GeneroID é a chave estrangeira de Genero para livro
+    }
+}
